2c6f3ff4f0ce68ca3a0bc2fe50565a9b563e07cd
Icyubahiro-Jay-P <151947328+Icyubahiro-Jay-P@users.noreply.github.com>
2026-04-19 11:46:05 +0200

    Update API_URL to include clients endpoint

--- src/api/clientServices.js
+++ src/api/clientServices.js
@@ -1,7 +1,7 @@
 // src/api/clientServices.js
 import axios from 'axios';
 
-const API_URL = 'https://aido-backend-h6gd.onrender.com';
+const API_URL = 'https://aido-backend-h6gd.onrender.com/api/clients';
 
 // Create a new client
 export const createClient = async (clientData) => {
